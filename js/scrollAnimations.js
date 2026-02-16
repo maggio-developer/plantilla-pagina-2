cd2a16b3d75437667dbd712162c61ab5ce4908f8
maggio-developer <maggio.mab@gmail.com>
2026-02-16 15:31:21 -0300

    Add files via upload

---
+++ js/scrollAnimations.js
@@ -0,0 +1,14 @@
+const articles = document.querySelectorAll(".reveal");
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        }
+    });
+}, {
+    threshold: 0.05
+});
+articles.forEach(article => {
+    observer.observe(article);
+});
